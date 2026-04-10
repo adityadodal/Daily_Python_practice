38f9a33164456a349fc3f0dcbae388253837e97d
Aditya Dodal <adityadodal@gmail.com>
2026-04-10 05:45:52 +0530

    It is really amazing to use conditional Statements and also i have learned new things and error solving

--- conditional_statement.py
+++ conditional_statement.py
@@ -1,2 +1,12 @@
-point = 18
-print(point)
+point = 8
+
+if point >= 18:
+    print("Result: Top of the table! Direct Qualifier 1")
+    print("Example: Gujrat Titans")
+elif point >= 14:
+    print(" Result: Playoffs je liye Qualify Kar Gaye ho! - Feels CSK or Mi like")
+elif point >= 10:
+    print("Result: Net Run Rate pe dependent ho gaye - Feels RCB or KKR like")
+else:
+    print("Result: Playoffs se bahar ho gaye - Feels PBKS or DC like")
+print("End of the program")
