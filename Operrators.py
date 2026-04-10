eaafb3b9cac6ca7f408d7bfeb5d7e45582c5c420
Aditya Dodal <adityadodal@gmail.com>
2026-04-10 05:32:02 +0530

    Day0 : Operators

---
+++ Operrators.py
@@ -0,0 +1,9 @@
+x = 10
+y = 20
+print(x + y)  # Addition
+print(x - y)  # Subtraction
+print(x * y)  # Multiplication
+print(x / y)  # Division
+print(x % y)  # Modulus
+print(x ** 2)  # Exponentiation
+print(x // 3)  # Floor Division
