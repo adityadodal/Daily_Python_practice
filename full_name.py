aa1a8324fc65a96530ebba003f1fc663e5b60c8d
Aditya Dodal <adityadodal@gmail.com>
2026-04-12 05:55:35 +0530

    full_name.py

---
+++ full_name.py
@@ -0,0 +1,26 @@
+first_name = "Ada"
+last_name = "Shelby"
+full_name = f"{first_name} {last_name}"
+print(full_name)
+
+first_name = "Ada"
+last_name = "shelby"
+full_name = f"{first_name} {last_name}"
+print(f"Hello, {full_name.title()}!")
+
+first_name = "ada"
+last_name = "shelby"
+full_name = f"{first_name} {last_name}"
+message = f"Hello, {full_name.title()}!"
+print(message)
+
+
+first_name = "Thomas"
+last_name = "Shelby"
+full_name = f"{first_name} {last_name}"
+print(full_name)
+
+first_name = "Thomas"
+last_name = "Shelby"
+full_name = f"{first_name} {last_name}"
+print(f"Hello, {full_name}!")
