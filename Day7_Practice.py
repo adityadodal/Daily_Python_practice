336469319518833bf9c68ad7d7d0377f239bda52
Aditya Dodal <adityadodal@gmail.com>
2026-04-16 05:04:13 +0530

    Day7

--- Day7_Practice.py
+++ Day7_Practice.py
@@ -12,3 +12,28 @@ def testing():
 
 # calling the function
 testing()
+
+
+def testing2(first_name, last_name):
+    print(f"Testing function : {first_name} {last_name}")
+    print("Testing Completed")
+    print(f"Hello {first_name} {last_name}")
+
+
+# calling the function
+testing2("John", "Doe")
+
+testing2("Aditya", "Dodal")
+
+
+# this function takes two numbers and returns their sum
+def add_numbers(num1, num2):
+    return num1 + num2
+
+
+# calling the function and storing the result in a variable
+result = add_numbers(5, 56)
+print(f"The sum of 5 and 56 is: {result}")
+
+# we can also automatically print the result without storing it in a variable
+print(f"The sum of 5 and 56 is: {add_numbers(5, 56)}")
