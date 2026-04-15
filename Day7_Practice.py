9ff447e30781077e450dc85de9a54838ab8b535d
Aditya Dodal <adityadodal@gmail.com>
2026-04-16 04:57:25 +0530

    Day7

---
+++ Day7_Practice.py
@@ -0,0 +1,14 @@
+# Built in functions
+
+# print() - prints the given object to the console
+# print("Hello, World!")
+
+# def - defines a function
+
+def testing():
+    print("Testing function")
+    print("Testing Completed")
+
+
+# calling the function
+testing()
