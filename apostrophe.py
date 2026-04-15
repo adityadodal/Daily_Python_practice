a8ebc845f164790ea89a37307dcaae3e1f81da24
Aditya Dodal <adityadodal@gmail.com>
2026-04-15 05:31:45 +0530

    Aphostrophe

---
+++ apostrophe.py
@@ -0,0 +1,12 @@
+message = "Don't forget to escape apostrophes in strings!"
+print(message)
+# Output: Don't forget to escape apostrophes in strings!
+# To include an apostrophe in a string, you can use double quotes to define the string:
+message = "Don't forget to escape apostrophes in strings!"
+print(message)
+# Output: Don't forget to escape apostrophes in strings!
+# Alternatively, you can use a backslash to escape the apostrophe:
+message = 'Don\'t forget to escape apostrophes in strings!'
+print(message)
+# Output: Don't forget to escape apostrophes in strings!
+# Both methods will allow you to include an apostrophe in your string without causing a syntax error.
