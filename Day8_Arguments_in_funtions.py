53530906dd888075d379b196b3e8a1ae386fb1ec
Aditya Dodal <adityadodal@gmail.com>
2026-04-18 04:54:09 +0530

    Day9_Types of arguments

---
+++ Day8_Arguments_in_funtions.py
@@ -0,0 +1,10 @@
+# IN THIS SESSION WE WILL PRACTICE THE CONCEPTS OF DAY 8
+# 1. Arguments in functions
+# 2. Types of arguments in functions
+
+# 1. Arguments in functions
+# Arguments are the values that we pass to a function when we call it.
+# They are used to provide input to the function so that it can perform its task.
+# In Python, we can define
+# a function that takes arguments using the def keyword followed by the function name and parentheses.
+# Inside the parentheses, we can specify the names of the arguments that the function will accept.
